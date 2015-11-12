89a2391ac56ed1ae26193e76ec55ce663137bd27
Ali Anwar <ali@alis-vaio.local>
2015-11-11 22:50:49 -0500

    Mail: Use sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,13 +76,5 @@ Wall::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = {:host => "wall.cumulus.ws"}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true,
-    :user_name => "defaultmailer4@gmail.com",
-    :password => "Qoaqloaqo1"
-  }
+  config.action_mailer.delivery_method = :sendmail
 end
