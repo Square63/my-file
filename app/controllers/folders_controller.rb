4452cad49f6ea0567ce9b5198c4c37bd73547e4d
Ali Anwar <ali@alis-vaio.local>
2015-11-15 02:37:50 -0500

    Folder: Display sub folders as well

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,4 +1,6 @@
 class FoldersController < ApplicationController
+  before_filter :get_parent
+
   def index
     @folder = current_user.folders.first || current_user.folders.create(name: "Main Folder")
     redirect_to @folder
@@ -11,13 +13,21 @@ class FoldersController < ApplicationController
   def create
     @folder = Folder.new folder_params
     @folder.user = current_user
+    @folder.parent = @parent
     @folder.save
+
+    redirect_to @folder.parent || @folder
   end
 
   private
 
   def folder_params
-    params.require(:upload).permit(:name, :parent_id)
+    return {} if params[:folder].blank?
+    params.require(:folder).permit(:name, :parent_id)
+  end
+
+  def get_parent
+    @parent = Folder.find params[:parent_id] if params[:parent_id].present?
   end
 
 end
